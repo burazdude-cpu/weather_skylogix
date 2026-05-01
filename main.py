3e1e72dd426a031e41fa9afaa4ed2b4071873fd6
burazdude-cpu <burazdude@gmail.com>
2026-05-02 00:12:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.ingest_weather import ingest_once, ensure_indexes
 from src.ingest_pg import run_pg_ingestion
 
+
 if __name__ == "__main__":
     ensure_indexes()
     ingest_once(None)
-    run_pg_ingestion()   # 👈 ADD THIS
+
+    run_pg_ingestion()
